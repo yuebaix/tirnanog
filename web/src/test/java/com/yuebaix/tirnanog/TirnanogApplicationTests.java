dedd5c8d8d876eec8f74682782ea941e774fbaed
yuebaix <yuebaix@outlook.com>
2021-08-23 00:12:17 +0800

    init project

---
+++ web/src/test/java/com/yuebaix/tirnanog/TirnanogApplicationTests.java
@@ -0,0 +1,11 @@
+package com.yuebaix.tirnanog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TirnanogApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
