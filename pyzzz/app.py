508911f6809225e0e1b86712aecb3fd1b2d8d370
yuebaix <yuebaix@outlook.com>
2021-08-26 20:03:37 +0800

    add request

--- pyzzz/app.py
+++ pyzzz/app.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from call import api
+# from router import create_app
 
 if __name__ == '__main__':
-    app.run()
+    # app = create_app()
+    # app.run()
+    api()
