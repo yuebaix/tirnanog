29eb92a538c98ff1a8360d01fba6bce5ebed4c05
yuebaix <yuebaix@outlook.com>
2021-08-29 14:58:45 +0800

    java call kotlin and scala

---
+++ javazzz/src/main/java/com/yuebaix/tirnanog/javazzz/App.java
@@ -0,0 +1,11 @@
+package com.yuebaix.tirnanog.javazzz;
+
+import com.yuebaix.tirnanog.kotlinzzz.TryKotlin;
+import com.yuebaix.tirnanog.scalazzz.TryScala;
+
+public class App {
+    public static void main(String[] args) {
+        new TryKotlin().call();
+        new TryScala().call();
+    }
+}
---
+++ kotlinzzz/src/main/kotlin/com/yuebaix/tirnanog/kotlinzzz/TryKotlin.kt
@@ -0,0 +1,7 @@
+package com.yuebaix.tirnanog.kotlinzzz
+
+class TryKotlin {
+    fun call() {
+        println("kotlin")
+    }
+}
