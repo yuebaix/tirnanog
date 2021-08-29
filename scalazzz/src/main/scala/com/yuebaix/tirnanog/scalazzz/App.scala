0930a543c5bd5860c8a36d20f8d769683039a62a
yuebaix <yuebaix@outlook.com>
2021-08-29 11:45:52 +0800

    add kotlin and scala module

---
+++ scalazzz/src/main/scala/com/yuebaix/tirnanog/scalazzz/App.scala
@@ -0,0 +1,7 @@
+package com.yuebaix.tirnanog.scalazzz
+
+object App {
+  def main(args: Array[String]): Unit = {
+    print("Scalazzz")
+  }
+}
--- web/src/main/java/com/yuebaix/tirnanog/TirnanogApp.java
+++ web/src/main/java/com/yuebaix/tirnanog/TirnanogApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TirnanogApplication {
+public class TirnanogApp {
     public static void main(String[] args) {
-        SpringApplication.run(TirnanogApplication.class, args);
+        SpringApplication.run(TirnanogApp.class, args);
     }
 }
